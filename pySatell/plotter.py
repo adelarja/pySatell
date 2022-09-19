228f314d816b39194c7040a835f8dedbae35e0c5
adelarja <adelarja@gmail.com>
2022-09-19 08:25:38 -0300

    Maybe it would be a good idea to have a ndvi cmap. In the plotter module there is a draft for the ndvi cmap.

---
+++ pySatell/plotter.py
@@ -0,0 +1,30 @@
+from matplotlib import colors
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+
+def inter_from_256(x):
+    return np.interp(x=x, xp=[0, 255], fp=[0, 1])
+
+
+cdict = {
+    'red': ((0.0, inter_from_256(169), inter_from_256(169)),
+            (0.1, inter_from_256(244), inter_from_256(244)),
+            (0.2, inter_from_256(253), inter_from_256(253)),
+            (0.3, inter_from_256(230), inter_from_256(230)),
+            (1, inter_from_256(112), inter_from_256(112))),
+    'green': ((0.0, inter_from_256(23), inter_from_256(23)),
+              (0.1, inter_from_256(109), inter_from_256(109)),
+              (0.2, inter_from_256(219), inter_from_256(219)),
+              (0.3, inter_from_256(241), inter_from_256(241)),
+              (1, inter_from_256(198), inter_from_256(198))),
+    'blue': ((0.0, inter_from_256(69), inter_from_256(69)),
+             (0.1, inter_from_256(69), inter_from_256(69)),
+             (0.2, inter_from_256(127), inter_from_256(127)),
+             (0.3, inter_from_256(146), inter_from_256(146)),
+             (1, inter_from_256(162), inter_from_256(162))),
+}
+
+ndvi_cmap = colors.LinearSegmentedColormap('ndvi_cmap', segmentdata=cdict)
