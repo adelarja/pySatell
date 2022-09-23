f6d93e5ffb44061d5ba18b5e610650d83a8d8e80
adelarja <adelarja@gmail.com>
2022-09-22 22:41:46 -0300

    Added the desired commands

---
+++ pySatell/cli.py
@@ -0,0 +1,9 @@
+"""Add a cli using typer in order to provide tools to make easier the process to obtain fields vegetation
+indexes.
+
+-> Command to check if there are new Sentinel 2 images giving a date.
+-> Command to download the desired images.
+-> Command to obtain geojson files using shp files.
+-> Command to process the NDVI (and more indexes) using the differents bands of the raster images.
+-> More...
+"""
